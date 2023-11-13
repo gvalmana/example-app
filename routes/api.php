7b1ae36b6bf3e3cce405aa2f4baddaae41dd11cb
Lead Backend <leadback@vixidev.com>
2023-11-13 14:08:24 -0500

    se agrega y se elimina por ID

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,9 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get('/', [PostListController::class, 'index'])->name('posts.index');
         Route::get('/select-2-list', [PostListController::class, 'select2list'])->name('posts.select_2_list');
         Route::get('/{id}', [PostListController::class, 'show'])->name('posts.show');
+        Route::delete('/delete-by-ids', [PostDeleteController::class, 'destroyByIds'])->name('posts.destroy-by-ids');
         Route::delete('/{id}', [PostDeleteController::class, 'destroy'])->name('posts.destroy');
         Route::patch('{id}/restore', [PostDeleteController::class, 'restore'])->name('posts.restore');
+        Route::patch('/restore-by-ids', [PostDeleteController::class, 'restoreByIds'])->name('posts.restore-by-ids');
     });
 });
